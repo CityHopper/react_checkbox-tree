7934d8be835f4b1a7e9b8f7c3b42df1cf6bb8614
young <peacestrokes@gmail.com>
2022-04-07 16:40:14 +0900

    Update .gitignore and delete .idea

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import {useState} from "react";
+import {CheckboxSwitch} from "./CheckboxTree";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [checkboxes, setCheckboxes] = useState([
+            {
+                label: "모든 과일",
+                name: "all",
+                value: false,
+                parent: false,
+                children: [
+                    {
+                        label: "사과",
+                        name: "apple",
+                        value: false,
+                        parent: "all",
+                        children: [
+                            {
+                                label: "빨간 사과",
+                                name: "apple-red",
+                                value: false,
+                                parent: "apple",
+                                children: false
+                            },
+                            {
+                                label: '초록 사과',
+                                name: 'apple-green',
+                                value: false,
+                                parent: "apple",
+                                children: false
+                            }
+                        ]
+                    },
+                    {
+                        label: '바나나',
+                        name: 'banana',
+                        value: false,
+                        children: false,
+                        parent: "all"
+                    },
+                    {
+                        label: '자몽',
+                        name: 'grapefruit',
+                        value: false,
+                        children: false,
+                        parent: "all"
+                    },
+                ]
+            }
+        ]
+    )
+
+    return (
+        <>
+            {/*{*/}
+            {/*    checkboxes.map((c, i) => (*/}
+            {/*        */}
+            {/*    ))*/}
+            {/*}*/}
+            모든 과일
+            <CheckboxSwitch
+                checkboxArray={checkboxes}
+                setCheckboxArray={setCheckboxes}
+                name={"all"}/>
+
+
+        </>
+    );
 }
 
 export default App;
---
+++ src/CheckboxTree.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+// 스위치 버튼 모양의 체크박스
+export function CheckboxSwitch(props) {
+    return (
+            <label className="switch">
+                <input type="checkbox"
+                       checked={checkboxValueFinder(props.checkboxArray, props.name)}
+                       onChange={() => props.setCheckboxArray(
+                           checkboxHandler(props.checkboxArray, props.name,
+                               !checkboxValueFinder(props.checkboxArray, props.name)))}
+                />
+            </label>
+    )
+}
+//
+
+///// 자식의 자식 체크박스까지 일괄 변경하는 함수들 (예시 참고: PassageBigData.js)
+// 특정 name의 체크박스 value 변경하는 함수
+export function checkboxHandler(array, name, bool) {
+    let arr = [...array];
+
+    // 자식들까지 모두 체크박스 value 변경
+    function checkboxChildrenLoop(ar, bl) {
+        for (let i of ar) {
+            i.value = bl;
+            if (i.children) {
+                checkboxChildrenLoop(i.children, bl)
+            }
+        }
+    }
+    // 부모 체크박스 value 검증
+    function checkboxParentValidation (ar, nm) {
+        for (let i of ar) {
+            if (i.name === nm) {
+                let array = i.children.map(s => s.value);
+                i.value = !!array.every(v => v === true);
+                if (i.parent) {checkboxParentValidation(arr, i.parent)}
+            } else {
+                if (i.children) {checkboxParentValidation(i.children, nm)}
+            }
+        }
+    }
+
+    // 특정 name의 value 변경
+    function checkboxNameLoop(ar, nm, bl) {
+        for (let i of ar) {
+            if (i.name === nm) {
+                i.value = bl;
+                // todo 부모 checkbox value 변경하기
+                if (i.parent) {
+                    checkboxParentValidation(arr, i.parent)
+                }
+                if (i.children) {checkboxChildrenLoop(i.children, bl)}
+            } else {
+                if (i.children) {checkboxNameLoop(i.children, nm, bl);}
+            }
+        }
+    }
+
+    checkboxNameLoop(arr, name, bool)
+    return arr
+}
+
+// 특정 name의 체크박스의 value(true/false)를 찾아주는 함수
+export function checkboxValueFinder(array, name) {
+    let result;
+
+    function checkboxLoop(ar, nm) {
+        for (let i of ar) {
+            if (i.name === nm) {
+                result = i.value;
+            } else {
+                if (i.children) {
+                    checkboxLoop(i.children, nm);
+                }
+            }
+        }
+    }
+
+    checkboxLoop(array, name);
+    return result
+}
